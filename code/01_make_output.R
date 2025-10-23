9f705b514bef8be7686ea80fa68dd70ae29ab005
Mia Yang <yangbufan0214@gmail.com>
2025-10-23 14:50:28 -0400

    new codes with 5 numbers

--- code/01_make_output.R
+++ code/01_make_output.R
@@ -33,3 +33,11 @@ saveRDS(
   random_numbers4,
   file = here::here("output", "random_numbers4.rds")
 )
+
+set.seed(5)
+random_numbers4 <- rbinom(100, 1, 0.25)
+
+saveRDS(
+  random_numbers5,
+  file = here::here("output", "random_numbers5.rds")
+)
